29434f0b24e90c4a0495cffd0fb8c6fb23e13bd8
giuseppecasaburi <enzobiondi0000@libero.it>
2025-03-27 22:56:18 +0100

    Creazione pagine necessarie e struttura index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- 1- Pagina di Ordine (index.php)
+- Creare un form HTML con i seguenti campi:
+    - Nome del cliente (name);
+    - Selezione del piatto (dish) → (es. Pizza, Pasta, Insalata);
+    - Quantità (quantity)
+- Il form deve inviare i dati con POST a process.php. -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    
+</body>
+</html>
